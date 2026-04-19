8a6997bcecbfac8624c4002deaea50fee9709a94
Matthew Richardson <97010005+06tron@users.noreply.github.com>
2026-04-19 15:41:32 -0400

    Starts on CharSeq.ts

--- src/CharSeq.ts
+++ src/CharSeq.ts
@@ -6,5 +6,28 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 class CharSeq {
+
+	str: String;
+
+	pullNumberArray() {
+		return (this.str.match(/-?(?:\d*\.)?\d+(?:[eE]-?\d+)?/g) ?? []).map(Number);
+	}
+
+	charSwap(arg = '7>5<(S)T') {
+		const mapMatch = new RegExp('[' + arg.replace(/[\\^\]-]/g, '\\$&') + ']', 'g');
+		this.str = this.str.replace(mapMatch, function (char) {
+			return arg.charAt(arg.length - arg.indexOf(char) - 1);
+		});
+		return this;
+	}
+
+	getNumbers(arg = ',') {
+		this.str = this.pullNumberArray().map(n => PathData.coord(n)).join(arg);
+		return this;
+	}
+
+	toWebpage(arg, fellback) {
+		
+	}
 	
 }
