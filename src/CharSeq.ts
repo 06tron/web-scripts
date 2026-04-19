590c2b029ce2623bc55c5ff25741d9a53aaf6ad0
Matthew Richardson <97010005+06tron@users.noreply.github.com>
2026-04-19 00:05:27 -0400

    Adds CharSeq.ts

---
+++ src/CharSeq.ts
@@ -0,0 +1,10 @@
+/* Copyright (c) 2026, Matthew Richardson
+(https://orcid.org/0009-0001-0977-2029).
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+class CharSeq {
+	
+}
